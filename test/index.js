014b386add2b0645afe432fbb8e428565f59bd26
Brian Ruddy <briancruddy@gmail.com>
2017-04-28 14:33:47 -0400

    accept a status code argument

--- test/index.js
+++ test/index.js
@@ -17,21 +17,30 @@ describe('no-slash express middleware', function () {
         res = {
             redirect: () => {}
         },
-        next = () => {};
+        next = () => {},
+        spiedNext = sinon.spy(next),
+        spiedRedirect = sinon.spy(res, 'redirect');
 
-    it('redirects urls that contain a trailing slash', () => {
-        let spiedNext = sinon.spy(next);
+    afterEach(() => {
+        spiedNext.reset();
+        spiedRedirect.reset();
+    });
 
+    it('does not redirect urls that do not have a trailing slash', () => {
         noSlash()(reqWithoutSlash, res, spiedNext);
 
         expect(spiedNext.called).to.be.true;
     });
 
-    it('redirects urls does not redirect urls without a trailing slash', () => {
-        let spiedRedirect = sinon.spy(res, 'redirect');
-
+    it('301 redirects urls with trailing slash when no argument is given', () => {
         noSlash()(reqWithSlash, res, next);
 
         expect(spiedRedirect.calledWith(301)).to.be.true;
+    });
+
+    it('acccepts statusCode arg for redirect', () => {
+        noSlash(302)(reqWithSlash, res, next);
+
+        expect(spiedRedirect.calledWith(302)).to.be.true;
     });
 });
