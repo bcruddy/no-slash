912ab11a96989771bd32d3ef73ee2a54af681d12
Brian Ruddy <briancruddy@gmail.com>
2017-05-18 07:00:31 -0400

    add tests for es5 module, modify test/app to make it simpler to test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,11 +1,13 @@
 const App = require('./app'),
-    request = require('supertest');
+    request = require('supertest'),
+    noSlash = require('../lib'),
+    es5NoSlash = require('../lib/es5');
 
 describe('the no-slash module', function () {
     let app;
 
     before(() => {
-        app = App();
+        app = App(noSlash);
     });
 
     describe('returns 200', function () {
@@ -67,7 +69,99 @@ describe('the no-slash module', function () {
         let app302;
 
         before(() => {
-            app302 = App(302);
+            app302 = App(noSlash, 302);
+        });
+
+        it('GET /path/', done => {
+            request(app302)
+                .get('/path/')
+                .expect('location', '/path')
+                .expect(302, done);
+        });
+
+        it('GET /path/?a=b', done => {
+            request(app302)
+                .get('/path/?a=b')
+                .expect('location', '/path?a=b')
+                .expect(302, done);
+        });
+
+        it('GET /path/more/?a=b', done => {
+            request(app302)
+                .get('/path/more/?a=b')
+                .expect('location', '/path/more?a=b')
+                .expect(302, done);
+        });
+    });
+});
+
+describe('the ES5 no-slash module', function () {
+    let app;
+
+    before(() => {
+        app = App(es5NoSlash);
+    });
+
+    describe('returns 200', function () {
+        it('GET /', done => {
+            request(app)
+                .get('/')
+                .expect(200, done);
+        });
+
+        it('POST /path', done => {
+            request(app)
+                .post('/path')
+                .expect(200, done);
+        });
+
+        it('GET /path', done => {
+            request(app)
+                .get('/path')
+                .expect(200, done);
+        });
+
+        it('GET /path?a=b', done => {
+            request(app)
+                .get('/path?a=b')
+                .expect(200, done);
+        });
+
+        it('GET /path/more?a=b', done => {
+            request(app)
+                .get('/path/more?a=b')
+                .expect(200, done);
+        });
+    });
+
+    describe('returns 301 redirect', function () {
+        it('GET /path/', done => {
+            request(app)
+                .get('/path/')
+                .expect('location', '/path')
+                .expect(301, done);
+        });
+
+        it('GET /path/?a=b', done => {
+            request(app)
+                .get('/path/?a=b')
+                .expect('location', '/path?a=b')
+                .expect(301, done);
+        });
+
+        it('GET /path/more/?a=b', done => {
+            request(app)
+                .get('/path/more/?a=b')
+                .expect('location', '/path/more?a=b')
+                .expect(301, done);
+        });
+    });
+
+    describe('returns 302 redirect', function () {
+        let app302;
+
+        before(() => {
+            app302 = App(es5NoSlash, 302);
         });
 
         it('GET /path/', done => {
